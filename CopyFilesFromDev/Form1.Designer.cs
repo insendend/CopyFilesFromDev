6d60284badabda10fdca0bf739ebb5aaa20ca9d9
and tag <br2q@mail.ru>
2017-06-16 19:54:45 +0300

    Add project files.

---
+++ CopyFilesFromDev/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace CopyFilesFromDev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.l_timecaption = new System.Windows.Forms.Label();
+            this.l_time = new System.Windows.Forms.Label();
+            this.lb_copiedfiles = new System.Windows.Forms.ListBox();
+            this.l_count = new System.Windows.Forms.Label();
+            this.lb_foundfiles = new System.Windows.Forms.ListBox();
+            this.l_attachinfo = new System.Windows.Forms.Label();
+            this.but_copy = new System.Windows.Forms.Button();
+            this.l_helpinfo = new System.Windows.Forms.Label();
+            this.l_timerinfo1 = new System.Windows.Forms.Label();
+            this.l_timerinfo2 = new System.Windows.Forms.Label();
+            this.timer_timeleft = new System.Windows.Forms.Timer(this.components);
+            this.timer_timework = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // l_timecaption
+            // 
+            this.l_timecaption.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_timecaption.AutoSize = true;
+            this.l_timecaption.Location = new System.Drawing.Point(533, 389);
+            this.l_timecaption.Name = "l_timecaption";
+            this.l_timecaption.Size = new System.Drawing.Size(67, 13);
+            this.l_timecaption.TabIndex = 0;
+            this.l_timecaption.Text = "time_caption";
+            this.l_timecaption.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_time
+            // 
+            this.l_time.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_time.AutoSize = true;
+            this.l_time.Location = new System.Drawing.Point(677, 389);
+            this.l_time.Name = "l_time";
+            this.l_time.Size = new System.Drawing.Size(26, 13);
+            this.l_time.TabIndex = 1;
+            this.l_time.Text = "time";
+            this.l_time.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // lb_copiedfiles
+            // 
+            this.lb_copiedfiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_copiedfiles.FormattingEnabled = true;
+            this.lb_copiedfiles.Location = new System.Drawing.Point(476, 57);
+            this.lb_copiedfiles.Name = "lb_copiedfiles";
+            this.lb_copiedfiles.Size = new System.Drawing.Size(236, 316);
+            this.lb_copiedfiles.TabIndex = 2;
+            this.lb_copiedfiles.Visible = false;
+            this.lb_copiedfiles.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_count
+            // 
+            this.l_count.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.l_count.AutoSize = true;
+            this.l_count.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_count.Location = new System.Drawing.Point(473, 39);
+            this.l_count.Name = "l_count";
+            this.l_count.Size = new System.Drawing.Size(66, 15);
+            this.l_count.TabIndex = 3;
+            this.l_count.Text = "count_files";
+            this.l_count.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // lb_foundfiles
+            // 
+            this.lb_foundfiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lb_foundfiles.FormattingEnabled = true;
+            this.lb_foundfiles.HorizontalScrollbar = true;
+            this.lb_foundfiles.Location = new System.Drawing.Point(12, 57);
+            this.lb_foundfiles.Name = "lb_foundfiles";
+            this.lb_foundfiles.Size = new System.Drawing.Size(164, 238);
+            this.lb_foundfiles.TabIndex = 4;
+            this.lb_foundfiles.Visible = false;
+            this.lb_foundfiles.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_attachinfo
+            // 
+            this.l_attachinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_attachinfo.Location = new System.Drawing.Point(12, 9);
+            this.l_attachinfo.Name = "l_attachinfo";
+            this.l_attachinfo.Size = new System.Drawing.Size(254, 32);
+            this.l_attachinfo.TabIndex = 5;
+            this.l_attachinfo.Text = "attach_msg";
+            this.l_attachinfo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // but_copy
+            // 
+            this.but_copy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.but_copy.Location = new System.Drawing.Point(12, 357);
+            this.but_copy.Name = "but_copy";
+            this.but_copy.Size = new System.Drawing.Size(164, 42);
+            this.but_copy.TabIndex = 6;
+            this.but_copy.Text = "copy";
+            this.but_copy.UseVisualStyleBackColor = true;
+            this.but_copy.Visible = false;
+            this.but_copy.Click += new System.EventHandler(this.button1_Click);
+            this.but_copy.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_helpinfo
+            // 
+            this.l_helpinfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.l_helpinfo.Location = new System.Drawing.Point(12, 309);
+            this.l_helpinfo.Name = "l_helpinfo";
+            this.l_helpinfo.Size = new System.Drawing.Size(191, 45);
+            this.l_helpinfo.TabIndex = 7;
+            this.l_helpinfo.Text = "helpinfo";
+            this.l_helpinfo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_timerinfo1
+            // 
+            this.l_timerinfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_timerinfo1.Location = new System.Drawing.Point(199, 57);
+            this.l_timerinfo1.Name = "l_timerinfo1";
+            this.l_timerinfo1.Size = new System.Drawing.Size(236, 46);
+            this.l_timerinfo1.TabIndex = 8;
+            this.l_timerinfo1.Text = "info_msg";
+            this.l_timerinfo1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // l_timerinfo2
+            // 
+            this.l_timerinfo2.AutoSize = true;
+            this.l_timerinfo2.Location = new System.Drawing.Point(199, 103);
+            this.l_timerinfo2.Name = "l_timerinfo2";
+            this.l_timerinfo2.Size = new System.Drawing.Size(40, 13);
+            this.l_timerinfo2.TabIndex = 9;
+            this.l_timerinfo2.Text = "timeleft";
+            this.l_timerinfo2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // timer_timeleft
+            // 
+            this.timer_timeleft.Interval = 1000;
+            this.timer_timeleft.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer_timework
+            // 
+            this.timer_timework.Enabled = true;
+            this.timer_timework.Interval = 1000;
+            this.timer_timework.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 411);
+            this.Controls.Add(this.l_timerinfo2);
+            this.Controls.Add(this.l_timerinfo1);
+            this.Controls.Add(this.l_helpinfo);
+            this.Controls.Add(this.but_copy);
+            this.Controls.Add(this.l_attachinfo);
+            this.Controls.Add(this.lb_foundfiles);
+            this.Controls.Add(this.l_count);
+            this.Controls.Add(this.lb_copiedfiles);
+            this.Controls.Add(this.l_time);
+            this.Controls.Add(this.l_timecaption);
+            this.MinimumSize = new System.Drawing.Size(650, 420);
+            this.Name = "Form1";
+            this.Text = "Copy Files From Device";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_timecaption;
+        private System.Windows.Forms.Label l_time;
+        private System.Windows.Forms.ListBox lb_copiedfiles;
+        private System.Windows.Forms.Label l_count;
+        private System.Windows.Forms.ListBox lb_foundfiles;
+        private System.Windows.Forms.Label l_attachinfo;
+        private System.Windows.Forms.Button but_copy;
+        private System.Windows.Forms.Label l_helpinfo;
+        private System.Windows.Forms.Label l_timerinfo1;
+        private System.Windows.Forms.Label l_timerinfo2;
+        private System.Windows.Forms.Timer timer_timeleft;
+        private System.Windows.Forms.Timer timer_timework;
+    }
+}
+
---
+++ CopyFilesFromDev/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+// Написать программу, которая при подключении нового диска осуществляет поиск всех файлов и 
+// копирование к себе в созданный каталог, имя которого должно соответствовать метке подключенного диска.
+
+namespace CopyFilesFromDev
+{
+    // делегат под сбор информации по флешке
+    delegate Dictionary <string, int> DelStat();
+
+    // делегат копирования
+    delegate List <string> DelCopy(string pattern);
+
+    public partial class Form1 : Form
+    {
+        // список подключенных дисков
+        private List<DriveInfo> lstDrives = null;
+        
+        // ID сообщ. добавл. диска
+        private const int ADD_DEVICE    = 0x8000;
+
+        // ID сообщ. удаления. диска
+        private const int REM_DEVICE    = 0x8004;
+
+        // время запуска программы
+        private readonly DateTime dtFirst = DateTime.Now;
+
+        // метка тома (диска)
+        private string strAddedDiskName = null;
+        private string strVolumeLabel = null;
+
+        public Form1()
+        {          
+            InitializeComponent();
+
+            // получаем список доступных дисков
+            this.lstDrives = new List<DriveInfo>(DriveInfo.GetDrives());
+
+            this.l_timecaption.Text = "Время работы программы: ";
+            this.l_time.Text = new TimeSpan().ToString();
+            this.l_count.Text = null;
+            this.l_attachinfo.Text = "Ожидание подключения диска...";
+            this.l_helpinfo.Text = null;
+            this.l_timerinfo1.Text = null;
+            this.l_timerinfo2.Text = null;
+            this.but_copy.Text = "Копировать";
+        }
+
+        // ловим сообщение от ОС
+        protected override void WndProc(ref Message msg)
+        {
+            if ((int)msg.WParam == ADD_DEVICE)
+            {
+                // был подключен диск
+                this.InitAddedDisk();
+
+                this.l_attachinfo.Text = string.Format("Подключён диск: {0}", this.strAddedDiskName);
+                this.timer_timeleft.Enabled = true;
+
+                if (this.strAddedDiskName != null)
+                {
+                    // настраиваем методы
+                    DelStat ds = ShowInfo;
+
+                    this.lb_foundfiles.Visible = true;
+
+                    // вывод информации о подключённом диске
+                    ds.BeginInvoke(Finish, ds);
+                }
+            }
+            // проверяем на отключение диска
+            else if ((int)msg.WParam == REM_DEVICE)
+            {
+                // получаем заново список дисков
+                List<DriveInfo> lstTmp = new List<DriveInfo>(DriveInfo.GetDrives());
+
+                // сверяем с ранее полученным списком дисков
+                foreach (var i in lstDrives)
+
+                    // проверяем на содержание нового списка дисков со старым
+                    if (!lstTmp.Exists(x => i.Name == x.Name))
+                    {
+                        lstDrives.Remove(i);
+                        break;
+                    }
+            }
+
+                base.WndProc(ref msg);
+        }
+
+        // поиск добавленного диска
+        private void InitAddedDisk()
+        {
+            try
+            {
+                // получаем заново список дисков
+                List<DriveInfo> lstTmp = new List<DriveInfo>(DriveInfo.GetDrives());
+
+                // сверяем с ранее полученным списком дисков
+                foreach (var i in lstTmp)
+
+                    // проверяем на содержание нового списка дисков со старым
+                    if (!this.lstDrives.Exists(x => x.Name == i.Name))
+                    {
+                        // определяем добавленный диск
+                        this.strAddedDiskName = i.Name;
+
+                        // метка тома
+                        this.strVolumeLabel = i.VolumeLabel;
+
+                        this.lstDrives.Add(i);
+                        break;
+                    }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // метод копирования файлов
+        private List <string> CopyOnlyParam(string ext)
+        {
+            var files = new List<string>();
+
+            try
+            {
+                // рекурсивный поиск по всем каталогам файлов с расширением из параметров
+                files = this.GetFiles(this.strAddedDiskName, ext);
+
+                if (files.Count > 0)
+                {
+                    // каталог с запущенной программой
+                    string rootFolder = Directory.GetCurrentDirectory();
+
+                    // имя компьютера, где была зарущена программа
+                    string namePC = Environment.MachineName;
+
+                    // перебираем все найденные TXT-файлы
+                    foreach (var sourcePath in files)
+                    {
+                        // формируем полный путь и имя файла назначения 
+                        string destPath = Path.Combine(Path.Combine(rootFolder, namePC), sourcePath.Remove(0, 2).Insert(0, this.strVolumeLabel));
+
+                        // создаем каталог, куда будем копировать файл
+                        Directory.CreateDirectory(Path.GetDirectoryName(destPath));
+
+                        // копирование
+                        File.Copy(sourcePath, destPath);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return files;
+        }
+
+
+        // рекурсивный поиск файлов, который игнорит системные файлы
+        private List <string> GetFiles(string path, string pattern)
+        {
+            var files = new List<string>();
+
+            try
+            {
+                // файлы из родительской папки
+                files.AddRange(Directory.GetFiles(path, pattern, SearchOption.TopDirectoryOnly));
+
+                // добавляем файлы из каждой внутренней папки
+                foreach (var directory in Directory.GetDirectories(path))
+                    files.AddRange(GetFiles(directory, pattern));
+            }
+            catch (UnauthorizedAccessException) { }
+
+            // возвращаем сформированный список файлов
+            return files;
+        }
+
+
+
+        // вывод списка расширений файлов из подключенного диска
+        private Dictionary <string, int> ShowInfo()
+        {
+            // коллекция для хранения расширений и их кол-ва
+            var dic = new Dictionary<string, int>();
+
+            try
+            {
+                // все файлы с полными путями
+                List <string> allFiles = this.GetFiles(this.strAddedDiskName, "*.*");
+
+                if (allFiles.Count > 0)
+                {
+                    // перебираем все файлы
+                    foreach (var fileName in allFiles)
+                    {
+                        // расширение файла
+                        string strExt = Path.GetExtension(fileName).ToUpper();
+
+                        // формируем словарь
+                        if (string.IsNullOrEmpty(strExt))
+                        {
+                            // метка "без расширения"
+                            string strKey = "Без расширения";
+
+                            // добавляем в словарь
+                            if (!dic.ContainsKey(strKey))
+                                dic.Add(strKey, 1);
+                            else
+                                dic[strKey]++;
+                        }
+                        else
+                        {
+                            // добавляем в словарь остальные расширения
+                            if (!dic.ContainsKey(strExt))
+                                dic.Add(strExt, 1);
+                            else
+                                dic[strExt]++;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return dic;
+        }
+
+        // работа таймера
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // выводим время работы программы в label
+            this.l_time.Text = (DateTime.Now - this.dtFirst.TimeOfDay).ToLongTimeString();
+        }
+
+        // завершение асинхронного вызова
+        private void Finish(IAsyncResult result)
+        {
+            try
+            {
+                DelStat ds = (DelStat)result.AsyncState;
+
+                // возвращаем результат - словарь найденных типов файлов
+                var dic = ds.EndInvoke(result);
+
+                // кол-во файлов
+                Action <string> act = (string s) => this.l_attachinfo.Text+= s;
+                this.Invoke(act, string.Format("\nОбнаружено {0} файлов", dic.Sum(x => x.Value)));
+
+                // помещаем словарь в листбокс
+                Action <string> act2 = (string s) => this.lb_foundfiles.Items.Add(s);
+
+                foreach (var ext in dic)
+                    this.Invoke(act2, string.Format("{0}-файлов: {1}", ext.Key, ext.Value));
+
+                Action <string> act3 = (string s) => this.l_helpinfo.Text = s;
+                this.Invoke(act3, "Выбирите тип файлов для копирования из списка выше и нажмите \"Копировать\"");
+
+                Action <bool> act4 = (bool b) => this.but_copy.Visible = b;
+                this.Invoke(act4, true);
+
+                Action<bool> act5 = (bool b) => this.lb_foundfiles.Visible = b;
+                this.Invoke(act4, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);       
+            }
+        }
+
+        // завершение асинхронного вызова
+        private void Finish2(IAsyncResult result)
+        {
+            try
+            {
+                DelCopy dc = (DelCopy)result.AsyncState;
+
+                // возвращаем результат - список скопированных файлов
+                var files = dc.EndInvoke(result);
+
+                // помещаем имена скопированных файлов в листбокс
+                Action <List <string>> act1 = (List <string> lst) => this.lb_copiedfiles.Items.AddRange(lst.ToArray());
+                this.Invoke(act1, files);
+
+                // кол-во файлов
+                Action <string> act2 = (string s) => this.l_count.Text = s;
+                this.Invoke(act2, string.Format("Скопировано {0} файлов", files.Count));
+
+
+                // помещаем скопированные файлы в листбокс
+                Action <bool> act3 = (bool b) => this.lb_copiedfiles.Visible = b;
+                this.Invoke(act3, true);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // автоматическое копирование при бездействии
+        private int iSec = 10;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.l_timerinfo1.Text = string.Format("Автоматическое копирование TXT-файлов начнется через {0}",--iSec);
+            this.l_timerinfo2.Text = "*чтобы отменить, кликнете по окну";
+
+            if (iSec == 0)
+            {
+                // имитируем клик по окну, чтобы остановить таймер и убрать текст
+                this.Form1_MouseClick(null, new MouseEventArgs(MouseButtons.Left, 1, 1, 1, 0));
+
+                // настраиваем делегат на метод
+                DelCopy dc = CopyOnlyParam;
+               
+                // запускаем копирование
+                IAsyncResult res = dc.BeginInvoke("*.txt", Finish2, dc);
+            } 
+        }
+
+        // кнопка "Копировать"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.lb_foundfiles.SelectedIndex == -1)
+                MessageBox.Show("Вы не выбрали ни один тип файлов для копирования");
+            else
+            {
+                // выделенный эл-т листбокса
+                string strTmp = (string)this.lb_foundfiles.Items[this.lb_foundfiles.SelectedIndex];
+
+                // выбирем из него расширение 
+                string strExtstrTmp = strTmp.Substring(strTmp.IndexOf('.'), strTmp.IndexOf('-') - strTmp.IndexOf('.')).Insert(0,"*").ToLower();
+
+                // настраиваем делегат на метод
+                DelCopy dc = CopyOnlyParam;
+
+                // запускаем копирование
+                IAsyncResult res = dc.BeginInvoke(strExtstrTmp, Finish2, dc);
+            }
+        }
+
+        // событие по нажатию на форму 
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            // прервать таймер
+            this.timer_timeleft.Enabled = false;
+
+            this.l_timerinfo1.Text = null;
+            this.l_timerinfo2.Text = null;
+        }
+    }
+}     
